6fd43979760cafdd89b88279ada8f32d4ae92a12
Deen Dabbagh <82648982+Deen-Dabbagh@users.noreply.github.com>
2023-03-03 21:16:54 +0400

    added arabic questions and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import Quiz from "react-quiz-component";
+import "./App.css";
 
-function App() {
+export const quiz = {
+  "disableSynopsis":true,
+  "appLocale": {
+    "landingHeaderText": "<questionLength> أسئلة",
+    "question": "السؤال",
+    "startQuizBtn": "إبدء الأسئلة",
+    "resultFilterAll": "الكل",
+    "resultFilterCorrect": "صحيح",
+    "resultFilterIncorrect": "خطئ",
+    "prevQuestionBtn": "السابق",
+    "nextQuestionBtn": "التالي",
+    "resultPageHeaderText": "لقد أكملت الاختبار. لقد حصلت على <correctIndexLength> من <questionLength> الأسئلة.",
+    "singleSelectionTagText": '',
+  "multipleSelectionTagText": '',
+  "pickNumberOfSelection": 'اختر <numberOfSelection>',
+
+  },
+  resultPagePoint: 'لقد حصلت على <correctPoints> من <totalPoints>.',
+
+  quizTitle: "أسئلة تجريبية",
+  quizSynopsis: "مهارات عامة",
+  questions: [
+    // السؤال الثاني
+    {
+      // السؤال
+      question: "ما هي عاصمة الإمارات العربية المتحدة؟",
+      // نوع السؤال
+      questionType: "text",
+      answerSelectionType: "single",
+      // الإجابات
+      answers: ["أبوظبي", "دبي", "الشارقة", "رأس الخيمة"],
+      // الإجابة الصحيحة
+      correctAnswer: "1",
+      messageForCorrectAnswer: "أحسنت، الجواب صحيح!",
+      // الإجابة الخاطئة
+      messageForIncorrectAnswer: "الجواب غير صحيح",
+      // شرح الإجابة
+      explanation: "عاصمة الإمارات العربية المتحدة هي أبوظبي",
+      point: "1"
+    },
+    {
+      // السؤال
+      question: "ما هي العملة الرسمية للمملكة المتحدة؟",
+      // نوع السؤال
+      questionType: "text",
+      answerSelectionType: "single",
+      // الإجابات
+      answers: ["الدولار الأمريكي", "اليورو", "الجنيه الإسترليني", "الفرنك السويسري"],
+      // الإجابة الصحيحة
+      correctAnswer: "2",
+      messageForCorrectAnswer: "أحسنت، الجواب صحيح!",
+      // الإجابة الخاطئة
+      messageForIncorrectAnswer: "الجواب غير صحيح",
+      // شرح الإجابة
+      explanation: "العملة الرسمية للمملكة المتحدة هي الجنيه الإسترليني",
+      point: "1"
+    },
+    {
+      // السؤال
+      question: "كم عدد الدول الأعضاء في الاتحاد الأوروبي؟",
+      // نوع السؤال
+      questionType: "text",
+      answerSelectionType: "single",
+      // الإجابات
+      answers: ["28", "27", "26", "25"],
+      // الإجابة الصحيحة
+      correctAnswer: "1",
+      messageForCorrectAnswer: "أحسنت، الجواب صحيح!",
+      // الإجابة الخاطئة
+      messageForIncorrectAnswer: "الجواب غير صحيح",
+      // شرح الإجابة
+      explanation: "يتكون الاتحاد الأوروبي حالياً من 27 دولة عضو",
+      point: "1"
+    }
+    
+  ]
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quiz-container">
+      <Quiz quiz={quiz} shuffle={true} showInstantFeedback={true} />
     </div>
   );
 }
-
-export default App;
